6cf66e9869c253f0acc9ce9252782ee567cd5fbe
Pratyaksh <pratyaksh1594@gmail.com>
2025-08-06 13:36:09 +0530

    added header & footer

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
+import Header from "@/components/ui/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,15 @@ export default function RootLayout({ children }) {
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          {/* header */}
+          <Header />
+          <main className = "min-h-screen">{children}</main>
+          {/*footer*/}
+          <footer className = "bg-muted/50 py-12">
+            <div className="container mx-auto px-4 text-center text-grey-200">
+              <p>Made with 💗 by Pratyaksh</p>
+            </div>
+          </footer>
         </ThemeProvider>
       </body>
     </html>
---
+++ components/ui/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function header() {
+  return (
+    <div className='bg-gray-800 text-white p-4 text-center'>
+      Header 
+    </div>
+  )
+}
+
+export default header
