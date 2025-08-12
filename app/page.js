119e1a769b281a896722a75359ed985470821b25
Pratyaksh <pratyaksh1594@gmail.com>
2025-08-12 19:32:37 +0530

    added hero.jsx

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import HeroSection from "@/components/hero";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-    <h1 className="text-4xl font-bold mb-6">Welcome to Guidely AI</h1>
-    <Button>click me</Button>
+    <div>
+    <HeroSection />
     </div>
   );
 }
